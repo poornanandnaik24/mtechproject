37626f12ce2786418c350f0e8d78c952cf3f016f
poornanandnaik24 <poornanandnaik24@gmail.com>
2018-04-12 11:37:39 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,13 @@
+package lab;
+
+class Dog{
+	  static int cube(int x){
+	  return x*x*x;
+	  }
+
+	  public static void main(String args[]){
+		  //Dog f=new Dog();
+	  int result=cube(5);
+	  System.out.println(result);
+	  }
+	}
